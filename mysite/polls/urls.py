0b5d2280b691cebea5c123b270a7fb68866a3a96
Amareh <amareh.alibarreh@up-set.co>
2021-07-22 20:52:57 +0300

    PLS

--- mysite/polls/urls.py
+++ mysite/polls/urls.py
@@ -3,6 +3,8 @@ from django.conf.urls import url
 from . import views
 from .models import Question
 
+app_name = 'blog'
+
 urlpatterns = [
     path('', views.index, name='index'),
     path('<str:question_label>', views.question_page, name='question'),
